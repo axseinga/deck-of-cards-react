d41f1e9e72c957fa03c668c2bbfc57d6c0dca861
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-10-12 17:43:02 +0100

    finish functionality

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
+import Deck from "./Deck";
 
 function App() {
-    const [deck, setDeck] = useState(null);
-    const [drawn, setDrawn] = useState({});
-
-    const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
-
-    useEffect(() => {
-        async function getData() {
-            let deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
-            console.log(deck);
-            setDeck(deck.data);
-        }
-        getData();
-    }, []);
-
-    const getCard = async () => {
-        let deck_id = deck.deck_id;
-        try {
-            let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
-            let cardRes = await axios.get(cardUrl);
-            if (cardRes.data.remaining === 0) {
-                throw new Error("No cards");
-            }
-            console.log(cardRes.data);
-            let card = cardRes.data.cards[0];
-            setDrawn({
-                id: card.code,
-                image: card.image,
-                name: `${card.value} of ${card.suit}`,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    return (
-        <div className="App">
-            <h1>Card Dealer</h1>
-            <button onClick={() => getCard()}>Gimme a card</button>
-        </div>
-    );
+    return <Deck />;
 }
 
 export default App;
--- src/Deck.js
+++ src/Deck.js
@@ -1,25 +1,55 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "./Card";
 import "./App.css";
 
 function Deck() {
     const [deck, setDeck] = useState(null);
+    const [drawn, setDrawn] = useState([]);
+
+    const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
     useEffect(() => {
         async function getData() {
-            let deck = await axios.get(
-                "https://deckofcardsapi.com/api/deck/new/shuffle"
-            );
+            let deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
             console.log(deck);
             setDeck(deck.data);
         }
         getData();
     }, []);
 
+    const getCard = async () => {
+        let deck_id = deck.deck_id;
+        try {
+            let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+            let cardRes = await axios.get(cardUrl);
+            if (!cardRes.data.success) {
+                throw new Error("No cards");
+            }
+            console.log(cardRes.data);
+            let card = cardRes.data.cards[0];
+            setDrawn((prevState) => [
+                ...prevState,
+                {
+                    id: card.code,
+                    image: card.image,
+                    name: `${card.value} of ${card.suit}`,
+                },
+            ]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const cards = drawn.map((c) => (
+        <Card key={c.id} name={c.name} image={c.image} />
+    ));
+
     return (
         <div className="App">
             <h1>Card Dealer</h1>
-            <button>Gimme a card</button>
+            <button onClick={() => getCard()}>Gimme a card</button>
+            {cards}
         </div>
     );
 }
